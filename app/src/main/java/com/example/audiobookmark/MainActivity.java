1b4093c19f0ba77da3cf1e90aade1167dc9cb35c
MShekow <s2mashek@uni-bonn.de>
2019-10-05 17:56:05 +0200

    Some experiments with MediaController callbacks

--- app/src/main/java/com/example/audiobookmark/MainActivity.java
+++ app/src/main/java/com/example/audiobookmark/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.audiobookmark;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.Observer;
@@ -8,16 +10,27 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.media.MediaMetadata;
+import android.media.session.MediaController;
+import android.media.session.MediaSession;
+import android.media.session.MediaSessionManager;
+import android.media.session.PlaybackState;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.audiobookmark.receiver.AudioBroadcastReceiver;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private BookmarkViewModel viewModel;
+    static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +56,77 @@ public class MainActivity extends AppCompatActivity {
                 adapter.setWords(words);
             }
         });
+
+        MediaSessionManager mm = (MediaSessionManager) this.getSystemService(
+                Context.MEDIA_SESSION_SERVICE);
+        List<MediaController> controllers = mm.getActiveSessions(
+                new ComponentName(getApplicationContext(), NotificationListenerExampleService.class));
+        MediaController doggCatcherMediaController = null;
+        for (MediaController controller : controllers) {
+            if (controller.getPackageName().contains("spotify")) {
+                doggCatcherMediaController = controller;
+                break;
+            }
+        }
+
+        if (doggCatcherMediaController != null) {
+            Log.d(TAG, "Controller: " + doggCatcherMediaController.toString());
+            doggCatcherMediaController.registerCallback(new MediaController.Callback() {
+                @Override
+                public void onSessionDestroyed() {
+                    Log.d(TAG, "onSessionDestroyed");
+                }
+
+                @Override
+                public void onSessionEvent(@NonNull String event, @Nullable Bundle extras) {
+                    Log.d(TAG, "onSessionEvent " + event);
+                }
+
+                @Override
+                public void onPlaybackStateChanged(@Nullable PlaybackState state) {
+                    Log.d(TAG, "onPlaybackStateChanged " + state.getState());
+
+                    MediaSessionManager mm = (MediaSessionManager) getSystemService(
+                            Context.MEDIA_SESSION_SERVICE);
+                    List<MediaController> controllers = mm.getActiveSessions(
+                            new ComponentName(getApplicationContext(), NotificationListenerExampleService.class));
+                    MediaController doggCatcherMediaController = null;
+                    for (MediaController controller : controllers) {
+                        if (controller.getPackageName().contains("spotify")) {
+                            doggCatcherMediaController = controller;
+                            break;
+                        }
+                    }
+
+                    if (doggCatcherMediaController != null) {
+                        Log.d(TAG, "Controller: " + doggCatcherMediaController.toString());
+                        doggCatcherMediaController.registerCallback(this);
+                        Log.d(TAG, "RE-Registered CB");
+                    }
+                }
+
+                @Override
+                public void onMetadataChanged(@Nullable MediaMetadata metadata) {
+                    Log.d(TAG, "onMetadataChanged");
+                }
+
+                @Override
+                public void onQueueChanged(@Nullable List<MediaSession.QueueItem> queue) {
+                    Log.d(TAG, "onQueueChanged");
+                }
+
+                @Override
+                public void onQueueTitleChanged(@Nullable CharSequence title) {
+                    Log.d(TAG, "onQueueTitleChanged");
+                }
+
+                @Override
+                public void onAudioInfoChanged(MediaController.PlaybackInfo info) {
+                    Log.d(TAG, "onAudioInfoChanged");
+                }
+            });
+            Log.d(TAG, "Registered CB");
+        }
     }
 
     @Override
--- app/src/main/java/com/example/audiobookmark/receiver/AudioBroadcastReceiver.java
+++ app/src/main/java/com/example/audiobookmark/receiver/AudioBroadcastReceiver.java
@@ -1,17 +1,30 @@
 package com.example.audiobookmark.receiver;
 
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaMetadata;
+import android.media.session.MediaController;
+import android.media.session.MediaSession;
+import android.media.session.MediaSessionManager;
+import android.media.session.PlaybackState;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.audiobookmark.NotificationListenerExampleService;
 import com.example.audiobookmark.receiver.impl.DoggcatcherIntentProcessor;
 import com.example.audiobookmark.receiver.impl.SpotifyIntentProcessor;
+
+import java.util.List;
 
 public class AudioBroadcastReceiver extends BroadcastReceiver {
     static final String GENERAL_ACTION = "com.android.music.playstatechanged";
     static final String TAG = "AudioBroadcastReceiver";
+    static boolean hasRegistered = false;
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -28,6 +41,83 @@ public class AudioBroadcastReceiver extends BroadcastReceiver {
                     className = value.getClass().getName();
                 } catch (Exception e) {}
                 Log.d(TAG, String.format("%s %s (%s)", key, strVal, className));
+            }
+        }
+
+        final Context test = context;
+
+        if (false && !AudioBroadcastReceiver.hasRegistered) {
+            MediaSessionManager mm = (MediaSessionManager) context.getSystemService(
+                    Context.MEDIA_SESSION_SERVICE);
+            List<MediaController> controllers = mm.getActiveSessions(
+                    new ComponentName(context, NotificationListenerExampleService.class));
+            MediaController doggCatcherMediaController = null;
+            for (MediaController controller : controllers) {
+                if (controller.getPackageName().contains("spotify")) {
+                    doggCatcherMediaController = controller;
+                    break;
+                }
+            }
+
+            if (doggCatcherMediaController != null) {
+                Log.d(TAG, "Controller: " + doggCatcherMediaController.toString());
+                doggCatcherMediaController.registerCallback(new MediaController.Callback() {
+                    @Override
+                    public void onSessionDestroyed() {
+                        Log.d(TAG, "onSessionDestroyed");
+                    }
+
+                    @Override
+                    public void onSessionEvent(@NonNull String event, @Nullable Bundle extras) {
+                        Log.d(TAG, "onSessionEvent " + event);
+                    }
+
+                    @Override
+                    public void onPlaybackStateChanged(@Nullable PlaybackState state) {
+                        Log.d(TAG, "onPlaybackStateChanged " + state.getState());
+
+                        MediaSessionManager mm = (MediaSessionManager) test.getSystemService(
+                                Context.MEDIA_SESSION_SERVICE);
+                        List<MediaController> controllers = mm.getActiveSessions(
+                                new ComponentName(test, NotificationListenerExampleService.class));
+                        MediaController doggCatcherMediaController = null;
+                        for (MediaController controller : controllers) {
+                            if (controller.getPackageName().contains("spotify")) {
+                                doggCatcherMediaController = controller;
+                                break;
+                            }
+                        }
+
+                        if (doggCatcherMediaController != null) {
+                            Log.d(TAG, "Controller: " + doggCatcherMediaController.toString());
+                            doggCatcherMediaController.registerCallback(this);
+                            Log.d(TAG, "RE-Registered CB");
+                            AudioBroadcastReceiver.hasRegistered = true;
+                        }
+                    }
+
+                    @Override
+                    public void onMetadataChanged(@Nullable MediaMetadata metadata) {
+                        Log.d(TAG, "onMetadataChanged");
+                    }
+
+                    @Override
+                    public void onQueueChanged(@Nullable List<MediaSession.QueueItem> queue) {
+                        Log.d(TAG, "onQueueChanged");
+                    }
+
+                    @Override
+                    public void onQueueTitleChanged(@Nullable CharSequence title) {
+                        Log.d(TAG, "onQueueTitleChanged");
+                    }
+
+                    @Override
+                    public void onAudioInfoChanged(MediaController.PlaybackInfo info) {
+                        Log.d(TAG, "onAudioInfoChanged");
+                    }
+                });
+                Log.d(TAG, "Registered CB");
+                AudioBroadcastReceiver.hasRegistered = true;
             }
         }
 
